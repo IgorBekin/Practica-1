05a7b900647133412837ec5d0c84e8e3ece01b7c
igorBekin <igor_bekin@mail.ru>
2023-02-20 15:56:05 +0300

    Initial Commit

---
+++ Practica 1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Practica 1
+//
+//  Created by Игорь Бекин on 20.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
