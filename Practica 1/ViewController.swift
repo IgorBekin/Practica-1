75f8f9043452dba29ed4719a1390f2f39d7cc4fe
igorBekin <igor_bekin@mail.ru>
2023-02-21 11:47:28 +0300

    Выполнение домашнего задания

--- Practica 1/ViewController.swift
+++ Practica 1/ViewController.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var a = 0
+    @IBOutlet weak var countLable: UILabel!
+    @IBOutlet weak var pressButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func appButton(_ sender: Any) {
+        a += 1
+        countLable.text = "Значение счетчика: \(a)"
+    }
+    
 
 }
 
